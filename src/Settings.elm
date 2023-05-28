bbf239cdb184a173894f685d9a50cf649e0647e6
Dowland Aiello <dowlandaiello@gmail.com>
2023-05-28 13:00:20 -0700

    Make messages page easier to read on mobile.

--- src/Settings.elm
+++ src/Settings.elm
@@ -14,8 +14,8 @@ viewAboutSection : Html Msg
 viewAboutSection =
     div [ class "section" ]
         [ h2 [ class "sectionHeader" ] [ text "App Info" ]
-        , p [] [ text "App Version: 0.85.0" ]
-        , p [] [ text "Last Updated: 5/27/23" ]
+        , p [] [ text "App Version: 0.87.0" ]
+        , p [] [ text "Last Updated: 5/28/23" ]
         ]
 
 
